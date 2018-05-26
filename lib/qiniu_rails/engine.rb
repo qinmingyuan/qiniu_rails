a462007af5e03d379c4fa3bed5e1950986a23872
qinmingyuan <mingyuan0715@foxmail.com>
2018-05-26 11:16:05 +0800

    only prepend for service is qiniu

--- lib/qiniu_rails/engine.rb
+++ lib/qiniu_rails/engine.rb
@@ -3,7 +3,9 @@ module QiniuRails
 
     initializer 'qiniu_rails.variant' do
       require 'active_storage/variant'
-      ActiveStorage::Variant.prepend QiniuRails::Variant
+      if ActiveStorage::Blob.service.is_a?(ActiveStorage::Service::QiniuService)
+        ActiveStorage::Variant.prepend QiniuRails::Variant
+      end
     end
 
   end
