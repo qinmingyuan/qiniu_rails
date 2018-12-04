0b68707d82c150b4fa35c276f8e2eeed5751c58e
qinmingyuan <mingyuan0715@foxmail.com>
2018-12-04 14:03:44 +0800

    direct upload for qiniu

--- lib/active_storage/service/qiniu_service.rb
+++ lib/active_storage/service/qiniu_service.rb
@@ -98,6 +98,7 @@ module ActiveStorage
     end
 
     def url_for_direct_upload(key, expires_in:, content_type:, content_length:, checksum:)
+      content_length = 4194304 > content_length ? content_length : 4194304
       instrument :url, key: key do |payload|
         url = Qiniu::Config.up_host(bucket) + "/mkblk/#{content_length}"
         payload[:url] = url
