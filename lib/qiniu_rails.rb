5d77f520d4202b280af41975b8d3f8740c225bf6
qinmingyuan <mingyuan0715@foxmail.com>
2019-12-06 23:34:11 +0800

    qiniu rails

--- lib/qiniu_rails.rb
+++ lib/qiniu_rails.rb
@@ -9,4 +9,3 @@ require_relative 'active_storage/analyzer/qiniu_image_analyzer'
 require_relative 'active_storage/analyzer/qiniu_video_analyzer'
 
 autoload :QiniuHelper, 'qiniu_rails/qiniu_helper'
-autoload :Sprockets, 'qiniu_rails/sprockets'
